f00c5b3b3626db543dd2adba7d8846c0503921db
Bobbyloujo <bobbyloujo@gmail.com>
2014-09-15 15:07:37 -0400

    Added template project
    This is a very simple project that contains basic code for a BobEngine
    game with one BobView, one Room, and one GameObject.

---
+++ Examples/Template/src/bobby/engine/template/MainActivity.java
@@ -0,0 +1,29 @@
+package bobby.engine.template;
+
+import android.content.Intent;
+import android.os.Bundle;
+import bobby.engine.bobengine.BobActivity;
+import bobby.engine.bobengine.SplashActivity;
+
+public class MainActivity extends BobActivity {
+	
+	// Data
+	private GameView gameView;                                 // The View that shows the GameObjects. Alternatively, you could
+	                                                           //     create a GameView in a layout with other views if you want
+	                                                           //     to use other views (TextView, WebView, ButtonView... etc)
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		/* Show the splash screen */
+		Intent splash = new Intent(this, SplashActivity.class);
+		startActivity(splash);
+		
+		/* Set up the GameView */
+		gameView = new GameView(this);
+		
+		/* Tell Android which view to display */
+		setContentView(gameView);
+	}
+}
