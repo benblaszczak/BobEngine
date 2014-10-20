6cf29e220f721e15ae91e604e10ba41a77206d0f
Bobbyloujo <bobbyloujo@gmail.com>
2014-10-20 18:10:18 -0400

    Updated Licensing information
    BobEngine is available under the GNU Lesser General Public License
    version 2.1

--- BobEngine 1.0 Thingama/Library Project/BobEngine/src/bobby/engine/bobengine/Touch.java
+++ BobEngine 1.0 Thingama/Library Project/BobEngine/src/bobby/engine/bobengine/Touch.java
@@ -1,3 +1,23 @@
+/**
+ * BobEngine - 2D game engine for Android
+ * 
+ * Copyright (C) 2014 Benjamin Blaszczak
+ * 
+ * BobEngine is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser Public License
+ * version 2.1 as published by the free software foundation.
+ * 
+ * BobEngine is provided without warranty; without even the implied
+ * warranty of merchantability or fitness for a particular 
+ * purpose. See the GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with BobEngine; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth
+ * Floor, Boston, MA 02110-1301 USA
+ * 
+ */
+
 package bobby.engine.bobengine;
 
 import android.annotation.SuppressLint;
